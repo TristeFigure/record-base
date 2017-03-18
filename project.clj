bfcca08d761c82f6fad8fbd09a336fa4de627c5f
TristeFigure <funtain@me.com>
2017-03-18 13:41:29 +0100

    Bump shuriken to 0.9.0

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojars.tristefigure/shuriken "0.3.0"]])
+                 [org.clojars.tristefigure/shuriken "0.9.0"]])
